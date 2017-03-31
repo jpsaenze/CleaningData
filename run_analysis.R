5a2d56f1e057c935092bbe7a03eb0354fd6ae2a8
jpsaenze <jpsaenze@gmail.com>
2017-03-30 22:25:26 -0700

    second commit

--- run_analysis.R
+++ run_analysis.R
@@ -16,18 +16,23 @@ activity_lables <- read.delim(paste(getwd(), "/UCI HAR Dataset/activity_labels.t
 features <- read.delim(paste(getwd(), "/UCI HAR Dataset/features.txt", sep=""), sep= " ", header = FALSE)
 
 #combining the data
-
 set_subject <- rbind(test_set_subject,train_set_subject)
 set_data <- rbind(test_set_data,train_set_data)
 set_set_activity <- rbind(test_set_activity,train_set_activity)
 full_set <- cbind(set_subject,set_set_activity,set_data)
 
-#select the columns to keep and fixes the lables and activity names
-selected_features_index <- c(1,2,2+ features[grep("mean|std",features$V2) ,1])
+#select the columns to keep and fixes the activity names
+selected_features_index <- c(1,2,2+ features[grep("mean\\(|std\\(",features$V2) ,1])
 full_set_reduce <-full_set[,selected_features_index]
-names(full_set_reduce) <- c( "Subject", "Activity", as.vector(features[grep("mean|std",features$V2) ,2]))
+names(full_set_reduce) <- c( "Subject", "Activity", as.vector(features[grep("mean\\(|std\\(",features$V2) ,2]))
 full_set_reduce$Activity <- recode(full_set_reduce$Activity, "1" = "WALKING", "2" = "WALKING_UPSTAIRS", 
                                    "3" = "WALKING_DOWNSTAIRS", "4" = "SITTING", "5"= "STANDING", "6"= "LAYING")
 
-write.csv(full_set_reduce)
-summarize(full_set_reduce, mean())
+## Optional to write the fileintermediate data set to file
+## write.csv(full_set_reduce, paste(getwd(),"/full_set_reduce.csv", sep=""))
+
+full_set_reduce <- group_by(full_set_reduce, Subject, Activity)
+tidy_data <- summarize_each(full_set_reduce, funs(median))
+tidy_data <- gather(tidy_data,"Measurements", "Mean", 3:68)
+write.csv(tidy_data, paste(getwd(),"/tidy_data.csv", sep=""))
+rm(list = ls())

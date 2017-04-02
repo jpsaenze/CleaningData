3cf6c9411e9ae9156262e47500c8c58e8a4a0492
jpsaenze <jpsaenze@gmail.com>
2017-04-01 18:19:09 -0700

    Corrections

--- run_analysis.R
+++ run_analysis.R
@@ -1,6 +1,9 @@
 ## run_analysis.R
 ## This files run the operations based on the README.txt
 
+library(dplyr)
+library(tidyr)
+
 #test data
 test_set_subject <- read.csv(paste(getwd(), "/UCI HAR Dataset/test/subject_test.txt",sep=""),sep = " ", header = FALSE)
 test_set_data <- read.fwf(paste(getwd(), "/UCI HAR Dataset/test/x_test.txt", sep=""), widths = rep(16,561))
@@ -21,10 +24,12 @@ set_data <- rbind(test_set_data,train_set_data)
 set_set_activity <- rbind(test_set_activity,train_set_activity)
 full_set <- cbind(set_subject,set_set_activity,set_data)
 
-#select the columns to keep and fixes the activity names
+#select the columns to keep only mean() and std() containing columns
 selected_features_index <- c(1,2,2+ features[grep("mean\\(|std\\(",features$V2) ,1])
 full_set_reduce <-full_set[,selected_features_index]
 names(full_set_reduce) <- c( "Subject", "Activity", as.vector(features[grep("mean\\(|std\\(",features$V2) ,2]))
+
+#Changing the lable of the activities
 full_set_reduce$Activity <- recode(full_set_reduce$Activity, "1" = "WALKING", "2" = "WALKING_UPSTAIRS", 
                                    "3" = "WALKING_DOWNSTAIRS", "4" = "SITTING", "5"= "STANDING", "6"= "LAYING")
 
@@ -33,6 +38,9 @@ full_set_reduce$Activity <- recode(full_set_reduce$Activity, "1" = "WALKING", "2
 
 full_set_reduce <- group_by(full_set_reduce, Subject, Activity)
 tidy_data <- summarize_each(full_set_reduce, funs(median))
-tidy_data <- gather(tidy_data,"Measurements", "Mean", 3:68)
+
+
+## Next line uncommented for producing long format 
+## tidy_data <- gather(tidy_data,"Measurements", "Mean", 3:68)
 write.csv(tidy_data, paste(getwd(),"/tidy_data.csv", sep=""))
 rm(list = ls())
